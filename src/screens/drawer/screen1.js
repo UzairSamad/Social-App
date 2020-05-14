46d7d7f4307cbb887db86a8981448803118d03f4
M.Uzair <uzair.iu17@gmail.com>
2020-05-14 13:28:01 +0500

    setup innitial screens and tabs

---
+++ src/screens/drawer/screen1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class screen1 extends Component {
+    render() {
+        return (
+            <View>
+                <Text> screen1 </Text>
+            </View>
+        )
+    }
+}
