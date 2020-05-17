7724bf814b2d55285859268475d317fe6caba0cc
M.Uzair <uzair.iu17@gmail.com>
2020-05-18 01:53:51 +0500

    added more styling

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet ,TouchableOpacity} from 'react-native'
+import { Text, View,StyleSheet} from 'react-native'
 
 export default class LoginScreen extends Component {
     
-    
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Login </Text>
-                <TouchableOpacity>
-                    <Text onPress={() => { this.props.navigation.navigate('Auth') }}>Create an account?</Text>
-                </TouchableOpacity>
+                <Text style={styles.greetings}>{"Hello again \n Welcomeback,"} </Text>
+                <View style={styles.erorMessage}>
+                    <Text>Error</Text>
+                </View>
+           
             </View>
         )
     }
 }
+
+
 const styles = StyleSheet.create({
     container:{ 
         flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+       
+    },
+    greetings:{
+        marginTop:32,
+        fontSize:20,
+        fontWeight:"400",
+        textAlign:"center" 
+    },
+    erorMessage:{
+        height:72,
+        alignItems:"center",
+        justifyContent:"center",
+        marginHorizontal:30
+
     }
 })
-
