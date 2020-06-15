1918ea5f6e062fa0c746532a3b2c0730cb76aeb2
M.Uzair <uzair.iu17@gmail.com>
2020-06-15 23:55:31 +0500

    added notificagtion screen

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,19 +1,47 @@
 
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, StatusBar } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native'
+import notifications from './notificationdata'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class NotificationScreen extends Component {
+
+    renderNotifications = post => {
+        return (
+            <View style={styles.feedItem}>
+            <Image source={post.avatar} style={styles.avatar} />
+            <View style={{ flex: 1 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View>
+                        <Text style={styles.name}>{post.name}</Text>
+                        {/* <Text style={styles.timestamp}>{}</Text> */}
+                    </View>
+                    <Text style={styles.timestamp}>{post.timestamp}</Text>
+                </View>
+                <View>
+                {post.text && <Text style={styles.post} style={{marginTop:5}}> {post.text}</Text>}
+    
+    {post.image && <Image source={post.image} style={styles.avatar} resizeMode="cover" />}
+                </View>
+                
+            </View>
+        </View>
+        )
+    }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
-                    
-                    <Text style={{color:'white', fontSize:20,  fontWeight:'5px'}}>Notifications</Text>
+                    <Text style={{ color: 'white', fontSize: 15, fontWeight: '3px' }}>Posts</Text>
                 </View>
-                <View style={styles.inner} >
 
-                    <Text> NotificationScreen 3 here </Text>
-                </View>
+                <FlatList
+                    style={styles.feed}
+                    data={notifications}
+                    renderItem={({ item }) => this.renderNotifications(item)}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}
+                />
             </View>
 
         )
@@ -22,22 +50,62 @@ export default class NotificationScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
+
     },
     header: {
-     borderBottomColor:'black',
-     borderBottomWidth:0.2,
-     shadowOpacity:20,
-     shadowColor:'grey',
-     padding:15,
-     backgroundColor:'#E9446A',
-     alignItems:'center',
+        borderBottomColor: 'black',
+        borderBottomWidth: 0.2,
+        shadowOpacity: 20,
+        shadowColor: 'grey',
+        padding: 15,
+        backgroundColor: '#E9446A',
+        alignItems: 'center',
 
-     },
-    inner:{
+    },
+    inner: {
         flex: 1,
-        justifyContent:'center',
-        alignItems:'center',
-       
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    feed: {
+        marginHorizontal: 16
+
+    },
+    feedItem: {
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        padding: 8,
+        flexDirection: 'row',
+        marginVertical: 8
+    },
+    avatar: {
+        width: 38,
+        height: 38,
+        borderRadius: 18,
+        marginRight: 14
+    },
+    timestamp: {
+        fontSize: 11,
+        color: '#C4C6CE',
+        marginTop: 4
+    },
+    name: {
+        fontSize: 15,
+        color: '#454d65',
+        fontWeight: "500"
+
+    },
+    post: {
+        marginTop: 16,
+        fontSize: 14,
+        color: '#838899'
+
+    },
+    postImage: {
+        width: undefined,
+        height: 150,
+        borderRadius: 5,
+        marginVertical: 16
     }
 })
---
+++ src/screens/notificationdata.js
@@ -0,0 +1,79 @@
+const notifications = [
+    {
+        id:'1',
+        name:'Muhammad Uzair',
+        text:'Hey , Muhammad we have new security options for you',
+        timestamp:'9 minutes ago',
+        avatar: require('../images/react-native.png'),
+    },
+    {
+        id:'2',
+        name:'Murshid',
+        text:'Invited you for a pubg' ,
+        timestamp:'2 hours ago',
+        avatar: require('../images/murshid.jpg'),
+    },
+    {
+        id:'2',
+        name:'Sinan Lakhani',
+        text:'sent you gift for candycrush' ,
+        timestamp:'just now',
+        avatar: require('../images/react-native.png'),
+    }
+    ,
+    {
+        id:'3',
+        name:'Muhammad Arshad Khan',
+        text:'liked your photo' ,
+        timestamp:'yestarday',
+        avatar: require('../images/profile1.jpg'),
+        image: require('../images/image1.webp')
+    },
+    {
+        id:'4',
+        name:'Muhammad Arshad Khan',
+        text:'Commented on a video you shared' ,
+        timestamp:'yestarday',
+        avatar: require('../images/profile1.jpg'),
+        
+    },
+    {
+        id:'5',
+        name:'Murshid',
+        text:'Shared your post' ,
+        timestamp:'3 days ago',
+        avatar: require('../images/profile1.jpg'),
+    },
+    {
+        id:'6',
+        name:'Muhammad Arshad Khan',
+        text:'liked your photo' ,
+        timestamp:'yestarday',
+        avatar: require('../images/profile1.jpg'),
+        image: require('../images/image1.webp')
+    },
+    {
+        id:'7',
+        name:'Sinan Lakhani',
+        text:'tagged you in a image' ,
+        timestamp:'yestarday',
+        avatar: require('../images/profile1.jpg'),
+        image: require('../images/image1.webp')
+    },
+    {
+        id:'8',
+        name:'Murshid',
+        text:'wish murshid birthday ' ,
+        timestamp:'yestarday',
+    },
+    {
+        id:'9',
+        name:'Sinan Lakhani',
+        text:'Sinan shared a photo of yours' ,
+        timestamp:'yestarday',
+        avatar: require('../images/profile1.jpg'),
+    },
+    
+]
+
+export default notifications
